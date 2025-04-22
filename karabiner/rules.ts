5c1294249bd12c792845d78d38f21f0b7ee4e68e
alex-ri <riss.alex@yahoo.de>
2025-04-22 19:58:16 +0200

    add initial config for karabiner

---
+++ karabiner/rules.ts
@@ -0,0 +1,79 @@
+import fs from "fs";
+import { KarabinerRules } from "./types";
+import { createHyperSubLayers, app, open, rectangle, shell } from "./utils";
+
+const rules: KarabinerRules[] = [
+  // Define the Hyper key itself
+  {
+    description: "Hyper Key (⌃⌥⇧⌘)",
+    manipulators: [
+      {
+        description: "Caps Lock -> Hyper Key",
+        from: {
+          key_code: "caps_lock",
+          modifiers: {
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            key_code: "left_shift",
+            modifiers: ["left_command", "left_control", "left_option"],
+          },
+          {
+            set_variable: {
+              name: "hyper",
+              value: 1,
+            },
+          },
+        ],
+        to_after_key_up: [
+          {
+            set_variable: {
+              name: "hyper",
+              value: 0,
+            },
+          },
+        ],
+        to_if_alone: [
+          {
+            key_code: "escape",
+          },
+        ],
+        type: "basic",
+      },
+    ],
+  },
+  ...createHyperSubLayers({
+    t: app("Ghostty"),
+    b: app("Brave Browser"),
+    v: app("Visual Studio Code"),
+    w: app("WhatsApp"),
+    m: app("Mail"),
+    c: app("Calendar"),
+    1: app("1Password"),
+    d: app("Discord"),
+    f: app("Finder"),
+  }),
+];
+
+fs.writeFileSync(
+  "karabiner.json",
+  JSON.stringify(
+    {
+      global: {
+        show_in_menu_bar: false,
+      },
+      profiles: [
+        {
+          name: "Default",
+          complex_modifications: {
+            rules,
+          },
+        },
+      ],
+    },
+    null,
+    2
+  )
+);
