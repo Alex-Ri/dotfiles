02d6f4b0fe810b3e3205719a43b1023d6d3010d4
alex-akq <alexander.riss@extern.edeka.de>
2025-12-21 09:49:57 +0100

    snacks config update

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,27 @@
+local grep_excludes = {
+  ".git",
+  "node_modules",
+  ".nuxt",
+  ".next",
+  "**/dist/**",
+  "**/package-lock.json",
+  "**/pnpm-lock.yaml",
+  "**/load-testing/**",
+}
+
 return {
   {
     "folke/snacks.nvim",
+    init = function()
+      -- open explorer on start when no files are passed
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          if #vim.fn.argv() == 0 then
+            require("snacks").picker("explorer")
+          end
+        end,
+      })
+    end,
     opts = {
       image = {
         enabled = true,
@@ -9,19 +30,25 @@ return {
         },
       },
       picker = {
+        layout = {
+          width = 0.9,
+          height = 0.9,
+        },
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+            layout = { auto_hide = { "input" } },
+            include = grep_excludes,
+          },
+        },
         hidden = true,
-        ignored = true,
-
-        exclude = {
-          ".git",
-          "node_modules",
-          ".nuxt",
-          ".next",
-        },
-        files = {
-          ignored = false,
-          hidden = true,
-        },
+        ignored = false,
+        exclude = grep_excludes,
+      },
+      files = {
+        ignored = false,
+        hidden = true,
       },
     },
   },
