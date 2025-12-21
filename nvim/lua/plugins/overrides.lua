506c6cd702ae1ae71b35ceb8d97f37ab968ccdbc
alex-akq <alexander.riss@extern.edeka.de>
2025-12-21 09:49:43 +0100

    border and noice changes

--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -3,6 +3,11 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      diagnostics = {
+        float = {
+          border = "rounded",
+        },
+      },
     },
   },
   {
@@ -10,5 +15,57 @@ return {
     opts = function(_, opts)
       opts.completion.menu.auto_show = false
     end,
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        border = "rounded",
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        float = {
+          border = "rounded",
+        },
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    require("noice").setup({
+      routes = {
+        -- disable crazy java lsp notifications
+        {
+          filter = {
+            event = "lsp",
+            kind = "progress",
+            find = "jdtls",
+          },
+          opts = { skip = true },
+        },
+      },
+      presets = {
+        lsp_doc_border = true,
+      },
+      views = {
+        hover = {
+          border = {
+            style = "rounded",
+          },
+        },
+      },
+    }),
   },
 }
