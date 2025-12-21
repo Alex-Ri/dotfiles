50b46f4b9572c76898e2b40e2a0b64d615c43abc
alex-akq <alexander.riss@extern.edeka.de>
2025-12-21 09:48:34 +0100

    add bufferline plugin

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      indicator = {
+        icon = "|",
+        style = "none",
+      },
+      separator_style = { "|", "|" }, -- left/right separator
+    },
+  },
+}
