9bbe6bb83bc157cb7454d119cbc6bf663986529e
alex-akq <alexander.riss@extern.edeka.de>
2025-12-21 09:49:15 +0100

    custom theme for lua line wip

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,9 +1,13 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
+    local custom_theme = require("lualine.themes.auto")
+    custom_theme.normal.c.bg = "none"
+    custom_theme.insert.c.bg = "none"
+    custom_theme.visual.c.bg = "none"
     require("lualine").setup({
       options = {
-        theme = "nordic",
+        theme = custom_theme,
       },
     })
   end,
