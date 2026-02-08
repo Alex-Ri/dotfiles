b7459dea682b688eca62fad9e488ab93edced26e
alex-ri <riss.alex@yahoo.de>
2026-02-08 15:09:51 +0100

    add nocture.nvim as colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,20 @@
 return {
-  {
-    "nord-dark",
-    dir = "~/Dev/nvim/nord-dark.nvim",
-    name = "nord-dark",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme nordic")
-      vim.cmd("doautocmd ColorScheme")
-    end,
+  "Alex-Ri/nocturne.nvim",
+  -- dir = "/Users/alex/Dev/nocturne.nvim",
+  priority = 1000,
+  opts = {
+    disable = {
+      background = true,
+      italic_comments = true,
+    },
+
+    plugins = {
+      telescope = false,
+      treesitter = false,
+    },
   },
+  config = function(_, opts)
+    require("nocturne").setup(opts)
+    vim.cmd.colorscheme("nocturne")
+  end,
 }
